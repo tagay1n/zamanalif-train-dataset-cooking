302027e7933b1de718dfa20e9c917657fa2d7bca
tagay1n <rkharisov.tt@gmail.com>
2026-07-10 19:41:57 +0300

    Add deterministic har hich qay stems

--- src/tatar_preannotator/word_export.py
+++ src/tatar_preannotator/word_export.py
@@ -1002,6 +1002,9 @@ NATIVE_PREFIX_REPLACEMENTS: tuple[tuple[str, str, str], ...] = (
     ("фәкать", "fäkat", "fäqat"),
     ("фәкыйрь", "fäkıyr", "fäqıyr"),
     ("хыянәт", "xıyanät", "xıyänät"),
+    ("һичкая", "hiçkaya", "hiçqaya"),
+    ("һичкай", "hiçkay", "hiçqay"),
+    ("һәркай", "härkay", "härqay"),
 )
 
 NATIVE_FRAGMENT_REPLACEMENTS: tuple[tuple[str, str, str], ...] = (
